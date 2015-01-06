b6a4a7a0050b3d4312b9b3a53d84677a3a2879f6
yannn12 <yannn12@gmail.com>
2015-01-06 19:03:33 +0200

    CMDParser interface

---
+++ data_protocol/binary_protocol/BinCMDParser.cpp
@@ -0,0 +1,23 @@
+/*
+ * BinCMDParser.cpp
+ *
+ *  Created on: 6 αιπε 2015
+ *      Author: Yaniv
+ */
+
+#include <data_protocol/binary_protocol/BinCMDParser.hpp>
+
+
+bool BinCMDParser::parsePacket(char* packet,int size,WorkDescription& result){
+	return false;
+}
+
+BinCMDParser::~BinCMDParser() {
+	// TODO Auto-generated destructor stub
+}
+
+BinCMDParser::BinCMDParser() {
+	// TODO Auto-generated constructor stub
+
+}
+
