ee50dbef275f6061e93ae6eef2126381ec419bc2
yannn12 <yannn12@gmail.com>
2015-01-15 02:06:08 +0200

    add Packet Factory

---
+++ data_protocol/abstract_datatype/PacketFactory.hpp
@@ -0,0 +1,23 @@
+/*
+ * PacketFactory.hpp
+ *
+ *  Created on: 15 αιπε 2015
+ *      Author: Yaniv
+ */
+
+#ifndef PACKETFACTORY_HPP_
+#define PACKETFACTORY_HPP_
+#include <data_protocol/abstract_datatype/EnergyPacket.hpp>
+#include <data_protocol/abstract_datatype/StatusPacket.hpp>
+#include <data_protocol/abstract_datatype/TempsPacket.hpp>
+
+class PacketFactory {
+public:
+	static PacketFactory *factory;
+	virtual EnergyPacket *createEnergyPacket() = 0;
+	virtual StatusPacket *createStatusPacket() = 0;
+	virtual TempsPacket *createTempsPacket() = 0;
+	virtual ~PacketFactory(){}
+};
+
+#endif /* PACKETFACTORY_HPP_ */
